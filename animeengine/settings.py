5982c18934a56560554ccf62a11c00d54acac943
chisomkun <chisom2much@Live.com>
2023-08-29 20:32:01 -0400

    Allowed hosts

--- animeengine/settings.py
+++ animeengine/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_b#t6q=wh+7ycq$!wz_9ng*doo56_pfeud*$k5f_a^yed_i8vl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

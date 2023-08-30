104308ff440f0578c8812b1313a81f755f38cc4e
chisomkun <chisom2much@Live.com>
2023-08-29 21:23:18 -0400

    Pod thread cursor issue

--- recommendation/engine/dbConfig.py
+++ recommendation/engine/dbConfig.py
@@ -1,14 +1,15 @@
 import sqlite3 
 import pandas as pd
 
-conn = sqlite3.connect("db.sqlite3")
 
 def animeConnect():
+    conn = sqlite3.connect("db.sqlite3")
     select = pd.read_sql_query("SELECT * FROM anime",conn)
     anime_df = pd.DataFrame(select, columns=['anime_id','name','genre','type','episodes','rating','members'])
     return anime_df
 
 def ratingConnect():
+    conn = sqlite3.connect("db.sqlite3")
     select = pd.read_sql_query("SELECT * FROM rating",conn)
     rating_df = pd.DataFrame(select, columns=['user_id','anime_id','rating'])
     return rating_df
